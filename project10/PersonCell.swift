77a1cf3a373069a45d7ea53414cd13e3a44e3f99
DenisenkoDenis1990 <denisenko1990@gmail.com>
2022-05-22 10:17:03 +0300

    Initial Commit

---
+++ project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  project10
+//
+//  Created by Denys Denysenko on 01.11.2021.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
